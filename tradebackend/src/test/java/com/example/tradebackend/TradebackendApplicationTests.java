f2528afcfc3c98dade314260cc8160c4fd19e10b
chong.rui.goh@citi.com <raygohcr@gmail.com>
2020-09-14 04:58:49 +0000

    InitialCommit

---
+++ tradebackend/src/test/java/com/example/tradebackend/TradebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tradebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradebackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
